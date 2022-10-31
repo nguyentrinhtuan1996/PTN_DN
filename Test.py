ab5bd29bba4c3a0241e204f9d6e33fad4d193721
Nguyễn Trịnh Tuấn <81601928+nguyentrinhtuan1996@users.noreply.github.com>
2022-10-31 21:51:22 +0700

    connect function ok

--- Test.py
+++ Test.py
@@ -1,32 +1,31 @@
-from modbus_tcp_server.network import ModbusTCPServer
-from modbus_tcp_server.data_source import BaseDataSource
+#!/usr/bin/env python3
 
-class CustomDB(BaseDataSource):
+"""
+Modbus/TCP server
+~~~~~~~~~~~~~~~~~
 
-    def __init__(self):
-        self.holding_registers = {1,2,3,4,5}
-        self.coils = {1,1,1,1,1}
+Run this as root to listen on TCP privileged ports (<= 1024).
 
-    def get_analog_input(self, unit_id: int, address: int) -> int:
-        return 0
+Add "--host 0.0.0.0" to listen on all available IPv4 addresses of the host.
+$ sudo ./server.py --host 0.0.0.0
+"""
 
-    def get_discrete_input(self, unit_id: int, address: int) -> bool:
-        return False
-
-    def get_holding_register(self, unit_id: int, address: int) -> int:
-        return self.holding_registers.get((unit_id, address), 0)
-
-    def get_coil(self, unit_id: int, address: int) -> bool:
-        return self.coils.get((unit_id, address), False)
-
-    def set_holding_register(self, unit_id: int, address: int, value: int) -> None:
-        self.holding_registers[(unit_id, address)] = value
-
-    def set_coil(self, unit_id: int, address: int, value: bool) -> None:
-        self.coils[(unit_id, address)] = value
+import argparse
+import logging
+from pyModbusTCP.server import ModbusServer
 
 
-c_db = CustomDB()
-
-at = ModbusTCPServer('localhost', 502, c_db).start()
-print("Modbus TCP/IP server started")
+# init logging
+logging.basicConfig()
+# parse args
+parser = argparse.ArgumentParser()
+parser.add_argument('-H', '--host', type=str, default='localhost', help='Host (default: localhost)')
+parser.add_argument('-p', '--port', type=int, default=502, help='TCP port (default: 502)')
+parser.add_argument('-d', '--debug', action='store_true', help='set debug mode')
+args = parser.parse_args()
+# logging setup
+if args.debug:
+    logging.getLogger('pyModbusTCP.server').setLevel(logging.DEBUG)
+# start modbus server
+server = ModbusServer(host=args.host, port=args.port)
+server.start()
