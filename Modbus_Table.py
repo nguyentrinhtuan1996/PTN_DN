d5f94f8cc44b23170c61e0b1ff9a4c7fd1f5f52a
Nguyễn Trịnh Tuấn <81601928+nguyentrinhtuan1996@users.noreply.github.com>
2022-11-06 18:08:14 +0700

    bus name

--- Modbus_Table.py
+++ Modbus_Table.py
@@ -3,8 +3,8 @@
 class Modbus_Table_Class():
     """
     there are 6 object:
-    - Bus: Bus Number, Bus Name
-    - Bus_Data: Bus_Number, Code, Udm, Normal, Normal_Vmin, Emergency_Vmax, Status
+    - Bus: Bus Number(1 int16), Bus Name(10 int16)
+    - Bus_Data: Bus_Number(1 int16), Code(1 int16), Udm(1 float32), Normal(1 float32), Normal_Vmin(1 float32) , Emergency_Vmax(1 float32), Status(1 bool)
     - Gen_Data: Bus_Number, Unit, Pgen, Qgen, Status
     - Line_Data: From_Bus_Number, To_Bus_Number, ID, Imax, R,X,G
     - Shunt_Data: Bus_Number, G_Shunt, B_Shunt, Status
@@ -21,12 +21,15 @@ class Modbus_Table_Class():
     QUANTITY_2_WINDING_DATA = 100
 
     # for bus object (11 x 2 byte) 
+    BUS_NAME_MAX_LENGTH = 10
+
     BUS_START_INPUT_REG = 0
-    BUS_END_INPUT_REG   = QUANTITY_BUS*11 - 1
+    BUS_END_INPUT_REG   = QUANTITY_BUS*(BUS_NAME_MAX_LENGTH + 1) - 1
 
     BUS_START_HOLDING_REG = 0
-    BUS_END_HOLDING_REG = QUANTITY_BUS*11 - 1
+    BUS_END_HOLDING_REG = QUANTITY_BUS*(BUS_NAME_MAX_LENGTH + 1) - 1
     
+    Bus_object_number = 0
     # for bus data object ( 12 x 2 bytes) quantity 100
     BUS_DATA_START_DISCRETE_INPUT = 0
     BUS_DATA_END_DISCRETE_INPUT = QUANTITY_BUS_DATA*1 -1
@@ -55,7 +58,7 @@ class Modbus_Table_Class():
 
     # for line data object
     LINE_DATA_START_DISCRETE_INPUT = GEN_DATA_END_DISCRETE_INPUT +1
-    LINE_DATA_END_DISCRETE_INOUT = LINE_DATA_START_DISCRETE_INPUT + QUANTITY_LINE_DATA*1 -1
+    LINE_DATA_END_DISCRETE_INPUT = LINE_DATA_START_DISCRETE_INPUT + QUANTITY_LINE_DATA*1 -1
 
     LINE_DATA_START_INPUT_REG = GEN_DATE_END_INPUT_REG +1
     LINE_DATA_END_INPUT_REG = LINE_DATA_START_INPUT_REG + QUANTITY_LINE_DATA*13 -1
@@ -67,11 +70,11 @@ class Modbus_Table_Class():
     LINE_DATA_END_HOLDING_REG = LINE_DATA_START_HOLDING_REG + QUANTITY_LINE_DATA*13 -1
 
     # for shunt data object
-    SHUNT_DATA_START_DISCRETE_INPUT = LINE_DATA_END_DISCRETE_INOUT +1
+    SHUNT_DATA_START_DISCRETE_INPUT = LINE_DATA_END_DISCRETE_INPUT +1
     SHUNT_DATA_END_DISCRETE_INPUT = SHUNT_DATA_START_DISCRETE_INPUT + QUANTITY_SHUNT_DATA*1 -1
 
     SHUNT_DATA_START_INPUT_REG = LINE_DATA_END_INPUT_REG +1
-    SHUNT_DATA_END_INOUT_REG = SHUNT_DATA_START_INPUT_REG + QUANTITY_SHUNT_DATA*3 -1
+    SHUNT_DATA_END_INPUT_REG = SHUNT_DATA_START_INPUT_REG + QUANTITY_SHUNT_DATA*3 -1
 
     SHUNT_DATA_START_COIL = LINE_DATA_END_COIL +1
     SHUNT_DATA_END_COIL = SHUNT_DATA_START_COIL + QUANTITY_SHUNT_DATA*1 -1
@@ -81,9 +84,9 @@ class Modbus_Table_Class():
 
     # for 2 winding data
     TWO_WINDING_DATA_START_DISCRETE_INPUT = SHUNT_DATA_END_DISCRETE_INPUT +1
-    TWO_WINDING_DATA_END_DISCRETE_INOUT = TWO_WINDING_DATA_START_DISCRETE_INPUT + QUANTITY_2_WINDING_DATA*1 -1 
+    TWO_WINDING_DATA_END_DISCRETE_INPUT = TWO_WINDING_DATA_START_DISCRETE_INPUT + QUANTITY_2_WINDING_DATA*1 -1 
 
-    TWO_WINDING_DATA_START_INPUT_REG = SHUNT_DATA_END_INOUT_REG +1
+    TWO_WINDING_DATA_START_INPUT_REG = SHUNT_DATA_END_INPUT_REG +1
     TWO_WINDING_DATA_END_INPUT_REG = TWO_WINDING_DATA_START_INPUT_REG + QUANTITY_2_WINDING_DATA*7 -1
 
     TWO_WINGDING_DATA_START_COIL = SHUNT_DATA_END_COIL +1
@@ -101,16 +104,39 @@ class Modbus_Table_Class():
         """
         init tables
         """
+        # these below array are raw data (16 bit registers) to load into modbus server
         #for CSV input
         self.discrete_inputs_number = 0
-        self.discrete_inputs_table = {}
+        self.discrete_inputs_table = []
         self.input_registers_number = 0
-        self.input_registers_table = {}
+        self.input_registers_table = []
         #for CSV output
         self.coils_number = 0
-        self.coils_table = {}
+        self.coils_table = []
         self.holding_registers_number = 0
-        self.holding_registers_table = {}
+        self.holding_registers_table = []
+
+        # init modbus table
+
+    def get_bus_name(self, bus_number):
+        """
+        get the bus name that is store in to bus number
+        - bus_number
+        - return (string) bus name
+        - return False if there is no this bus_number
+        """
+        for count in range (0, self.QUANTITY_BUS):
+            # calculate the address of bus number
+            bus_number_address = self.BUS_START_INPUT_REG + count*11
+            # found weather this bus number is existing 
+            if(self.input_registers_table[bus_number_address] == bus_number):
+                bus_name = ''
+                # read bus_name from modbus table
+                for count1 in range(1,self.BUS_NAME_MAX_LENGTH +1):
+                    bus_name = bus_name + chr((self.input_registers_table[bus_number_address + count1]))
+                return bus_name
+        # this bus number is not existing
+        return False
 
     def set_bus(self,bus_number, bus_name):
         """
@@ -126,14 +152,27 @@ class Modbus_Table_Class():
         if (len(bus_name) >10):
             bus_name = bus_name[:10]
         # var to check if this bus number is existing 
-        bus_number_existed = 0
-        for count in range(0, self.input_registers_number+1):
-            if (self.input_registers_table[count]
-        # parsing data into dictionaries
-        self.input_registers_table[1] = {}
-        self.QUAN
 
-        print(self.input_registers_table[1]["bus_name"])
+        for count in range (0, self.Bus_object_number):
+            # calculate the address of bus number
+            bus_number_address = self.BUS_START_INPUT_REG + count*11
+            # found weather this bus number is existing 
+            if(self.input_registers_table[bus_number_address] == bus_number):
+                # read bus_name from modbus table
+                count1 = 0
+                for letter_in_bus_name in bus_name:
+                    count1 =+1
+                    self.input_registers_table[bus_number_address + count1] = ord(letter_in_bus_name)
+
+                return True
+        # if there is no this bus number
+        self.input_registers_table.append(bus_number)
+
+        for count2 in range(0,len(bus_name)):
+            self.input_registers_table.append(ord(bus_name[count2]))
+        for count3 in range(len(bus_name),10):
+            self.input_registers_table.append(0)
+        print(self.input_registers_table)
         return True
 
     def set_bus_data(self,
@@ -182,6 +221,10 @@ class Modbus_Table_Class():
         pass
 
 # testing
+# array_X = [1]
+# print(array_X[1])
 if __name__ == '__main__':
     modbus_table =Modbus_Table_Class()
-    modbus_table.set_bus(101,'123456789012')
+    modbus_table.set_bus(101,'abcaa123123123')
+    print(modbus_table.get_bus_name(101))
+    # print(chr(64))
